db2e22761280762ae9bc96494fbce6f29f4ae802
Ritik Panchal <ritikpcl@gmail.com>
2023-11-28 02:24:00 +0530

    update-status-comp

--- src/components/Groups/Status/Status.js
+++ src/components/Groups/Status/Status.js
@@ -1,7 +1,13 @@
 import React, { useContext } from 'react';
 import { KanbanContext } from '../../../context/KanbanContext';
 import Card from '../../Card/Card';
-import './Status.css'; 
+import Dots from '../../../assets/icons/dots-three.svg'
+import Add from '../../../assets/icons/add.svg'
+import Todo from '../../../assets/icons/loading.svg'
+import Progress from '../../../assets/icons/progress2.png'
+import Done from '../../../assets/icons/done.png'
+import Cancel from '../../../assets/icons/cross.svg'
+import './Status.css';
 
 const Status = () => {
   const { state } = useContext(KanbanContext);
@@ -15,31 +21,90 @@ const Status = () => {
   return (
     <div className="status">
       <div className="status-column">
-        <h2>Backlog</h2>
+        <div className="status-column-heading">
+          <div className='status-column-heading-title'>
+            <img className='todo' src={Todo} alt="todo Icon" />
+            <p>Backlog<span className='numbers'>{filterTicketsByStatus('Backlog').length}</span></p>
+          </div>
+
+
+          <div className='status-column-heading-options'>
+            <img className='addIcon' src={Add} alt="Add Icon" />
+            <img className='threeDots' src={Dots} alt="Dots Icon" />
+          </div>
+
+        </div>
+
         {filterTicketsByStatus('Backlog').map((ticket) => (
           <Card key={ticket.id} ticket={ticket} />
         ))}
       </div>
 
       <div className="status-column">
-        <h2>To Do</h2>
-        {filterTicketsByStatus('Todo').map((ticket) => (
+        <div className="status-column-heading">
+          <div className='status-column-heading-title'>
+            <img className='todo' src={Todo} alt="todo Icon" />
+            <p>Todo<span className='numbers'>{filterTicketsByStatus('todo').length}</span></p>
+          </div>
+
+
+          <div className='status-column-heading-options'>
+            <img className='addIcon' src={Add} alt="Add Icon" />
+            <img className='threeDots' src={Dots} alt="Dots Icon" />
+          </div>
+
+        </div>
+
+        {filterTicketsByStatus('todo').map((ticket) => (
           <Card key={ticket.id} ticket={ticket} />
         ))}
       </div>
 
+
       <div className="status-column">
-        <h2>In Progress</h2>
+        <div className="status-column-heading">
+          <div className='status-column-heading-title'>
+            <img className='progress' src={Progress} alt="progress Icon" />
+            <p>In Progress<span className='numbers'>{filterTicketsByStatus('In Progress').length}</span></p>
+          </div>
+
+
+          <div className='status-column-heading-options'>
+            <img className='addIcon' src={Add} alt="Add Icon" />
+            <img className='threeDots' src={Dots} alt="Dots Icon" />
+          </div>
+
+        </div>
+
         {filterTicketsByStatus('In Progress').map((ticket) => (
           <Card key={ticket.id} ticket={ticket} />
         ))}
       </div>
 
       <div className="status-column">
-        <h2>Done</h2>
+        <div className="status-column-heading">
+          <div className='status-column-heading-title'>
+            <img className='done' src={Done} alt="Done Icon" />
+            <p>Done<span className='numbers'>{filterTicketsByStatus('Done').length}</span></p>
+          </div>
+
+
+          <div className='status-column-heading-options'>
+            <img className='addIcon' src={Add} alt="Add Icon" />
+            <img className='threeDots' src={Dots} alt="Dots Icon" />
+          </div>
+
+        </div>
         {filterTicketsByStatus('Done').map((ticket) => (
           <Card key={ticket.id} ticket={ticket} />
         ))}
+      </div>
+
+      <div className="status-column cancel-div">
+        <div className='status-column-heading-title'>
+          <img className='cancel' src={Cancel} alt="Cancel Icon" />
+          <p>Cancelled<span className='numbers'>0</span></p>
+        </div>
       </div>
     </div>
   );
