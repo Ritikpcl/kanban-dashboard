d6972ca72721e6f29861f740f1aea3313333c700
Ritik Panchal <ritikpcl@gmail.com>
2023-11-28 00:09:49 +0530

    fix-header

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Header/>
         <main>
           <Board />
-          {/* <Card/> */}
         </main>
       </div>
     </KanbanProvider>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { KanbanContext } from '../../context/KanbanContext';
+import Menu from '../../assets/icons/option.svg'
+import DownArrow from '../../assets/icons/down-arrow.svg'
+import UpArrow from '../../assets/icons/up-arrow.svg'
 import './Header.css';
 
 const Header = () => {
+  const [show, setShow] = useState(false)
   const { state, dispatch } = useContext(KanbanContext);
 
   const handleGroupingChange = (event) => {
@@ -15,26 +19,39 @@ const Header = () => {
 
   return (
     <header className="app-header">
-      <h1>Kanban Board</h1>
-
-      <div className="dropdown-container">
-        <label>
-          Group By:
-          <select value={state.groupingOption} onChange={handleGroupingChange}>
-            <option value="user">User</option>
-            <option value="status">Status</option>
-            <option value="priority">Priority</option>
-          </select>
-        </label>
-
-        <label>
-          Sort By:
-          <select value={state.sortingOption} onChange={handleSortingChange}>
-            <option value="priority">Priority</option>
-            <option value="title">Title</option>
-          </select>
-        </label>
+      <div >
+      <div className='display' onClick={() => setShow(!show)}>
+        <img className='menu' src={Menu} alt="Menu Icon" />
+        <p>Display</p>
+        {
+          show ? 
+          <img className='up-arrow' src={UpArrow} alt="Up Icon" /> : 
+          <img className='down-arrow' src={DownArrow} alt="Down Icon" />
+        }
+        
       </div>
+      </div>
+
+        {
+          show && <div className="dropdown-container">
+          <label className='label'>
+            <span className='group'>Grouping</span>
+            <select value={state.groupingOption} onChange={handleGroupingChange}>
+              <option value="user">User</option>
+              <option value="status">Status</option>
+              <option value="priority">Priority</option>
+            </select>
+          </label>
+
+          <label className='label'>
+          <span className='group'>Ordering</span>
+            <select value={state.sortingOption} onChange={handleSortingChange}>
+              <option value="priority">Priority</option>
+              <option value="title">Title</option>
+            </select>
+          </label>
+        </div> 
+        }
     </header>
   );
 };
